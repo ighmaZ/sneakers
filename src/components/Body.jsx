1c9398fed0bd27bda8194a77d30b58d86d4ca19e
ighmaz <ighmazcool@gmail.com>
2022-09-27 20:46:39 +0530

    styling done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -11,13 +11,14 @@ const Body = () => {
     <div className='flex flex-row'>
         <div className='flex flex-col'>
             <div className=''>
-                <img classname src={blue}  />
+                <img className="w-400 "src={blue}  />
             </div>
-            <div className='flex flex-row image '>
-<div><img src={blue} /></div>
-<div><img src={green} /></div>
-<div><img src={red} /></div>
-<div><img src={white} /></div>
+            <div className='flex flex-row image mt-20 mx-5 '>
+
+<img className="p-5 rounded-xl" src={blue} />
+<img className="p-5 rounded-xl" src={green} />
+<img className="p-5 rounded-xl" src={red} />
+<img className="p-5 rounded-xl" src={white} />
 
             </div>
         </div>
@@ -25,15 +26,16 @@ const Body = () => {
 
         
         <div className='m-10 mt-20 '>
-            <h3>SNEAKER COMPANY</h3>
-            <h1>FALL LIMITED EDITION SNEAKERS</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi tenetur corporis quisquam sint beatae consequuntur.</p>
-            <h1>$125</h1>
+            <h3 className='mt-5 font-semibold  text-orange-500 text-2xl'>SNEAKER COMPANY</h3>
+            <h1 className='mt-10 font-bold text-5xl'>FALL Limited Edition Shoes</h1>
+            <p className='mt-20  text-gray-400'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, esse. Excepturi quis ratione deleniti velit nam error aspernatur quam pariatur?</p>
+            <h1 className='mt-10 font-bold text-3xl'>$125</h1>
+
 <div className='flex flex-row mt-10'>
-    <button className='bg-lime-300 w-10 h-10 text-red-600'>-</button>
-    <h1 className='bg-lime-300 w-10 '>0</h1>
-    <button className='bg-lime-300 w-10 h-10 text-red-600'>+</button>
-    <button  className='bg-orange-300 w-40 h-10 text-white'>Add to cart</button>
+    <button className='bg-lime-300 w-10 h-10 text-red-600 text-3xl'>-</button>
+    <h1 className='bg-lime-300 w-10 text-3xl  text-center '>0</h1>
+    <button className='bg-lime-300 w-10 h-10 text-red-600 text-3xl'>+</button>
+    <button  className='ml-8 bg-orange-300 w-60 h-10 text-white shadow-2xl'>Add to cart</button>
 </div>
 
         </div>
